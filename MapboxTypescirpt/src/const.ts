3f10a294fc03dc9425c333af0a40f213415f7de1
NJ <nj.namju@gmail.com>
2022-11-06 10:35:25 -0500

    Update const.ts

--- MapboxTypescirpt/src/const.ts
+++ MapboxTypescirpt/src/const.ts
@@ -1,7 +1,7 @@
 import mapboxgl from "mapbox-gl";
 
 export namespace MapConst {
-  export const ACCESS_TOKEN = 'pk.eyJ1IjoiZGVzaWduanUiLCJhIjoiY2xhNGU0YWo4MDlhYzNwdHBwenVvang4eiJ9.P3sX4l_3KjdPeRRbVS1VVg'; // 'SET YOUR ACCESSTOKEN HERE!!!!'
+  export const ACCESS_TOKEN = 'SET YOUR ACCESSTOKEN HERE!!!!'
   export const POINT_TOKYO_STATION = new mapboxgl.LngLat( -71.093161, 42.358871);
 
   // Maps service | API | Mapbox - https://docs.mapbox.com/api/maps/#styles
